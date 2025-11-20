a3f4caa27bdad442d52873365240321de8fd4dbc
sky510 <121666880@qq.com>
2025-11-20 22:14:44 +0800

    Delete unnecessary files

--- start_server.bat
+++
@@ -1,4 +0,0 @@
-cd /d d:\code\AI_Code\Need_To_Do\need_to_do
-venv\Scripts\activate.bat
-python runserver.py
-pause
--- start_server.ps1
+++
@@ -1,3 +0,0 @@
-Set-Location -Path "d:\code\AI_Code\Need_To_Do\need_to_do"
-& "d:\code\AI_Code\Need_To_Do\need_to_do\venv\Scripts\Activate.ps1"
-python runserver.py
